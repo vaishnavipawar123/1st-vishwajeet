230e141c92f2879e314d28c2da45ae69fb5d33a8
vaishnavipawar123 <120191259+vaishnavipawar123@users.noreply.github.com>
2022-12-23 19:18:19 +0530

    Add files via upload

---
+++ v.py
@@ -0,0 +1,17 @@
+#global declarations....
+a=int(input("enter the value of a"))
+d=int(input("enter the value of d"))
+n=int(input("enter the value of n"))
+#end
+
+#S = n/2[2a + (n − 1) × d]
+a1=n/2
+a2=2*a
+a3=(n-1)*d
+a4=a2+a3
+a5=a4*a1
+#end
+
+#printing..
+print(a5)
+#end
